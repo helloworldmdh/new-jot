ef95b47ccabdd7fe1cbe0645a2f91cc9f18f4d0a
Harry-Power <harrypower2@gmail.com>
2022-01-20 15:40:24 +0000

    Added routing

---
+++ src/router/routes.js
@@ -0,0 +1,7 @@
+function loadPage (component) {
+    // '@' is aliased to src/components
+    return () => import(/* webpackChunkName:"[request]" */ `@/pages/${component}.vue`)
+}
+export default [
+    { path: '/', component: loadPage('Timetable') }
+]
