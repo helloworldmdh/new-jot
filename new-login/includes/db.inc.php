7e045d6b408d25a55c8ec4372e9d88cfe581e39b
Harry-Power <74599558+Harry-Power@users.noreply.github.com>
2022-01-20 14:30:49 +0000

    Delete new-login directory

--- new-login/includes/db.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbUser = "root";
-$dbPassword = "";
-$dbName = "userinfo";
-
-$connect = new mysqli('localhost', $dbUser, $dbPassword, $dbName) or die("Unable to connect to database.");
-
-echo "connection to database successful!";
