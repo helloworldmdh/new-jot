49e0a572985c65fe7829f015edea663d2d2152a6
Harry-Power <harrypower2@gmail.com>
2022-01-20 15:38:47 +0000

    Added routing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view></router-view>
+  <h1>HI</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory } from 'vue-router';
+import routes from './router/routes';
 
-createApp(App).mount('#app')
+
+let router = createRouter({
+    history:createWebHistory(),
+    routes: routes,
+});
+
+
+const app = createApp(App);
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router);
+app.mount('#app');
