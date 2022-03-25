de5e8aaa658993f4967c53c87bfb1ea940be1d2a
Harry-Power <harrypower2@gmail.com>
2022-03-25 14:06:41 +0000

    Updated styling of not found page
    Co-authored-by: DanielSGH <DanielSGH@users.noreply.github.com>

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,27 +1,85 @@
 <template>
-<base-card>
-  <h1>404: Page not found!</h1>
-  <h3>Please try one of our other pages:</h3>
-  <ul>
-    <li>
-      <router-link to="/">Home Page</router-link>
-    </li>
-    <li>
-      <router-link to="/timetable">Timetable Page</router-link>
-    </li>
-  </ul>
-</base-card>
+  <div class="landing_body">
+    <div class="header">
+      <router-link to="/aboutus" class="link">About the App</router-link>
+    </div>
+    <div class="main">
+      <div class="app_name">404: Page Not Found</div>
+      <div id="desc">Try going here:</div>
+      <base-button :to="$store.getters.isSignedIn ?  '/timetable' : '/'"><div id="login-text">{{ $store.getters.isSignedIn ? "Timetable" : "Log in"}}</div></base-button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  //
+};
 </script>
 
 <style scoped>
-li {
-  display: inline;
-  padding-left:10px;
+
+.landing_body {
+  background: url(../assets/img/landing.jpg) no-repeat;
+  height: 100vh;
+  background-size: auto auto;
+  color: #14142c;
+  background-color: #7acff7b0;
 }
-</style>
+
+.main {
+  justify-content: center;
+  padding: 4rem;
+  top: 5vh;
+  position: relative;
+  display: block;
+  border-radius: 1em;
+  box-shadow: 0px 5px 10px #6867a1;
+  width: 40%;
+  margin-top: 4vh;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: aliceblue;
+}
+
+.header {
+  height: 5rem;
+  width: 100%;
+  background-color: var(--accent-one);
+  box-shadow: 0px 2px 10px rgb(151, 151, 151);
+}
+
+.link {
+  color: white;
+  position: fixed;
+  font-size: 24px;
+  top: 1.25rem;
+  right: 2.5rem;
+  text-decoration: none;
+  font-family: "Montserrat", sans-serif;
+}
+
+.link:hover {
+  text-decoration: underline;
+}
+
+.app_name {
+  text-align: center;
+  margin-bottom: 4rem;
+  font-size: 58px;
+}
+
+#desc {
+  font-style: italic;
+  font-size: 32px;
+  font-weight: 300;
+  margin-bottom: 5%;
+}
+
+#login-text {
+  margin-top: 12px;
+}
+</style> 
+
+
+    
